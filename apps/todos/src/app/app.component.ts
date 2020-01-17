0d0874d83defbc831256ca4e8d74f7cc9982e1ca
Annie Huang <annie.hn@gmail.com>
2020-01-17 21:08:20 +1100

    Update app.component.ts and app.component.html to pass the addTodo cypress test.

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -12,4 +12,10 @@ interface Todo {
 export class AppComponent {
   title = 'todos';
   todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
 }
