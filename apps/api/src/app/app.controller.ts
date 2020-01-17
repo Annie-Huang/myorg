c1abe4d1ba642c715a8049d2ef9889546967ae15
Annie Huang <annie.hn@gmail.com>
2020-01-17 21:58:02 +1100

    Add logic in app.controller.ts and app.app.service.ts for nest api app

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
+// Controllers are responsible for implementing Http endpoints.
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // @Get()
+  // getData() {
+  //   return this.appService.getData();
+  // }
+
+  @Get('todos')
   getData() {
     return this.appService.getData();
+  }
+
+  @Post('addTodo')
+  addTodo() {
+    return this.appService.addTodo();
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+interface Todo {
+  title: string;
+}
+
+// Services in Nest are responsible for the business logic
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+  // getData(): { message: string } {
+  //   return { message: 'Welcome to api!' };
+  // }
+
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  getData(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
   }
 }
